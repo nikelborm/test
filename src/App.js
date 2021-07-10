9c5758ad0e25b36bf898f7e65d1b8d36ebb2c7cc
nikelborm <kolya007.klass@gmail.com>
2021-07-10 15:53:07 +0300

    assd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, PureComponent } from 'react';
 
-function App() {
+function FuncChild( props ) {
+  console.log( "FuncChild is called" );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
-  );
+  )
+}
+function FuncChildThatWillBeWrappedInMemo( props ) {
+  console.log( "FuncChildThatWillBeWrappedInMemo is called" );
+  return (
+    <div>
+    </div>
+  )
+}
+const FuncChildWithMemo = React.memo( FuncChildThatWillBeWrappedInMemo );
+
+class ClassChild extends Component {
+  constructor() {
+    super()
+    console.log("ClassChild constructor is called");
+  }
+  render() {
+    console.log("ClassChild render is called");
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+class PureClassChild extends PureComponent {
+  constructor() {
+    super()
+    console.log("PureClassChild constructor is called");
+  }
+  render() {
+    console.log("PureClassChild render is called");
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    console.log("App constructor is called the first and only once");
+    
+  }
+  componentDidMount() {
+    setTimeout(() => this.forceUpdate(), 10000)
+  }
+  render() {
+    console.log("App render is called");
+    return (
+      <div>
+        <FuncChild/>
+        <FuncChildWithMemo/>
+        <ClassChild/>
+        <PureClassChild/>
+      </div>
+    );
+  }
 }
 
 export default App;
